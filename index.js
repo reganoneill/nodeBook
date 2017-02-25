09925335914a54db6958b2ed0269ad0994acc0ff
Regan O'Neill <rkrotho@gmail.com>
2017-02-25 00:39:58 -0800

    request handling implemented

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 'use strict';
 
 const server = require('./server.js');
-const router = require('./router.js')
+const router = require('./router.js');
+const requestHandlers = require('./requestHandlers.js');
+
+//we require our requestHandler file in here, then pass methods from this
+//file to the server, which in turn passes them to the router. This allows multiple abstraction
+//layers to keep our separate files clean and pliable. Our router.js file is particularly reusable.
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
 
 
-server.start(router.route);
+server.start(router.route, handle);
--- router.js
+++ router.js
@@ -1,7 +1,14 @@
 'use strict';
 
-function route(pathname){
+const reqRouter = require('./requestHandlers.js');
+
+function route(handle, pathname){
   console.log(`About to route a request for ${pathname}`);
+  if(typeof handle[pathname] === 'function'){
+    handle[pathname]();
+  } else {
+    console.log(`No request handler found for ${pathname}`);
+  }
 };
 
 exports.route = route;
