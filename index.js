ce05ccfe97bb0373abcf2943f411e695d4d07be4
Regan O'Neill <rkrotho@gmail.com>
2017-02-25 15:56:28 -0800

    proving that an expensive task wont block other processes through request handling with response object

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const server = require('./server.js');
 const router = require('./router.js');
 const requestHandlers = require('./requestHandlers.js');
 
-//we require our requestHandler file in here, then pass methods from this
-//file to the server, which in turn passes them to the router. This allows multiple abstraction
-//layers to keep our separate files clean and pliable. Our router.js file is particularly reusable.
-
 var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
--- requestHandlers.js
+++ requestHandlers.js
@@ -5,7 +5,8 @@ var exec = require("child_process").exec;
 function start(response){
   console.log("Request handler 'start' was called.");
 
-  exec("ls -lah", function(error, stdout, stderr){
+  exec("find /", { timeout: 10000, maxBuffer: 20000*1024},
+    function(error, stdout, stderr){
     response.writeHead(200, {
       "Content-Type": "text/plain"
     });
