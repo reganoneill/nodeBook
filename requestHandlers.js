f57eab58340a6205c91c1fc03fb586dedeacf2e2
Regan O'Neill <rkrotho@gmail.com>
2017-02-25 15:51:19 -0800

    non blocking async passing server response through router and into request handlers for max efficiency

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,20 +1,25 @@
 'use strict';
 
-function start(){
+var exec = require("child_process").exec;
+
+function start(response){
   console.log("Request handler 'start' was called.");
 
-  function sleep(milliSeconds){
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
-  sleep(10000);
-  return "Hello Start!";
+  exec("ls -lah", function(error, stdout, stderr){
+    response.writeHead(200, {
+      "Content-Type": "text/plain"
+    });
+    response.write(stdout);
+    response.end();
+  });
+
 }
 
-function upload(){
+function upload(response){
   console.log("Request handler 'upload' was called.");
-  return "Hello Upload!"
-
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload!!!");
+  response.end();
 }
 
 exports.start = start;
--- router.js
+++ router.js
@@ -2,13 +2,17 @@
 
 const reqRouter = require('./requestHandlers.js');
 
-function route(handle, pathname){
+function route(handle, pathname, response){
   console.log(`About to route a request for ${pathname}`);
   if(typeof handle[pathname] === 'function'){
-    return handle[pathname]();
+     handle[pathname](response);
   } else {
     console.log(`No request handler found for ${pathname}`);
-    return "404 Not Found";
+    response.writeHead(400, {
+      "Content-Type": "text/plain"
+    });
+    response.write("404 Not Found");
+    response.end();
   }
 };
 
