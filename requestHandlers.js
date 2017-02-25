2fed7b4710b993bcd47cb79bdd40540b888abcc3
Regan O'Neill <rkrotho@gmail.com>
2017-02-25 11:07:52 -0800

    implement blocking in requestHandlers to throttle load time

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,12 +2,18 @@
 
 function start(){
   console.log("Request handler 'start' was called.");
-  return "Hello start request!";
+
+  function sleep(milliSeconds){
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+  }
+  sleep(10000);
+  return "Hello Start!";
 }
 
 function upload(){
   console.log("Request handler 'upload' was called.");
-  return "Hello upload request!"
+  return "Hello Upload!"
 
 }
 
