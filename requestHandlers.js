c11d5c3c14ffee97f2e7e4147a66eb7271f26678
Regan O'Neill <rkrotho@gmail.com>
2017-02-25 16:29:17 -0800

    added data and end listeners to the request object to handle POST requests - also logged POST body to console

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,14 +5,24 @@ var exec = require("child_process").exec;
 function start(response){
   console.log("Request handler 'start' was called.");
 
-  exec("find /", { timeout: 10000, maxBuffer: 20000*1024},
-    function(error, stdout, stderr){
-    response.writeHead(200, {
-      "Content-Type": "text/plain"
-    });
-    response.write(stdout);
+
+  var body =
+  '<html>'+
+     '<head>'+
+     '<meta http-equiv="Content-Type" content="text/html; '+
+     'charset=UTF-8" />'+
+     '</head>'+
+     '<body>'+
+       '<form action="/upload" method="post">'+
+         '<textarea name="text" rows="20" cols="60"></textarea>'+
+         '<input type="submit" value="Submit text" />'+
+       '</form>'+
+     '</body>'+
+   '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
     response.end();
-  });
 
 }
 
--- server.js
+++ server.js
@@ -5,10 +5,19 @@ const url = require('url');
 
 function start(route, handle){
   function onRequest(request,response){
+    var postDataBody = '';
     var pathname = url.parse(request.url).pathname;
     console.log(`Request for ${pathname} received.`);
 
-    route(handle, pathname, response);
+    request.setEncoding("utf8");
+    request.addListener("data", function(postDataChunk){
+      postDataBody += postDataChunk;
+      console.log("Received POST data chunk '" + postDataChunk + "''");
+    });
+    
+    request.addListener("end", function(){
+      route(handle, pathname, response, postDataBody);
+    });
 
   }
 http.createServer(onRequest).listen(8888);
